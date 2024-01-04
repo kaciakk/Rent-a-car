c59e91a262f9af22cc4f5a00473724cd3730d71a
kaciakk <wlomcj0@gmail.com>
2024-01-04 11:13:21 +0100

    backend serwer

--- frontend/src/Components/Login/Login.jsx
+++ frontend/src/Components/Login/Login.jsx
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios'; // Importuj axios
 
 export default function Login() {
+ 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/login', {email, password})
+      .then(result => { console.log(result)
+      if (result.data === "success") {
+      navigate('/')
+      }
+    })
+      .catch(err => console.log(err))
+}
   return (
+
     <div className='login-container'>
       <div className='login-container-inside'>
-        <h2>Loginnnn</h2>
+        <h2>Login</h2>
+        <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" name="email" />
+          <input
+           type="text" 
+           name="email" 
+           onChange={(e) => setEmail(e.target.value)}
+           />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
+          <input 
+          type="password" 
+          name="password"
+          onChange={(e) => setPassword(e.target.value)} />
         </div>
         <button type="submit">Login</button>
+        </form>
         <div>
           Don't have an account? <Link  to='/signup'>Create an account</Link>
         </div>
--- frontend/src/Components/Signup/Signup.jsx
+++ frontend/src/Components/Signup/Signup.jsx
@@ -1,25 +1,60 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './Signup.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import {faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from 'axios';
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
 
 export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('http://localhost:3001/register', { username, email, password })
+      .then(result => console.log(result),
+      navigate('/login'))
+      .catch(err => console.log(err));
+  };
+
   return (
     <div className='signup-container'>
       <div className='signup-container-inside'>
         <h2>Sign Up</h2>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" />
-        </div>
-        <button type="submit">Sign Up</button>
+        {/* Umieść formularz wewnątrz komponentu */}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              name="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
         <div>
           Already have an account? <Link to='/login'>Log in</Link>
         </div>
