c8e30dcaea2ef369e43eddb830ffbae481d26c84
kaciakk <wlomcj0@gmail.com>
2024-01-04 13:39:02 +0100

    add_new_registration_form_with_working_db

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,11 @@ const UserModel = require('./models/User')
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+  credentials: true // zezwalaj na wysyłanie zapytań z ciasteczkami (cookies) lub nagłówkami uwierzytelniającymi
+
+}));
 
 //Database connection with mongodb
 mongoose.connect("mongodb://127.0.0.1:27017/users")
